76b8c1c4db38b98e0ec7fd66a21e0e9635dd32f1
aryan-prog <mandelakalam12364@gmail.com>
2025-04-26 10:24:04 +0530

    Grid and Animations complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,9 +31,6 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
-          </p>
           <TextGenerateEffect
             words="Transforming Concepts into Seamless User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
@@ -43,7 +40,7 @@ const Hero = () => {
             Hi! I&apos;m Aryan, a Software Developer based in India.
           </p>
 
-          <a href="#about">
+          <a href="#projects">
             <MagicButton
               title="Show my work"
               icon={<FaLocationArrow />}
