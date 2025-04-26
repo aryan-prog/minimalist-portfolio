e6a156a5904281818fa6a62b1930db550e938fd7
aryan-prog <mandelakalam12364@gmail.com>
2025-04-26 12:02:46 +0530

    techStack added

---
+++ components/TechStack.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FloatingDock } from './ui/FloatingDock';
+import { techstack } from "@/data";
+
+const TechStack = () => {
+  return (
+    <div id="techstack"className="w-full py-20 px-4 sm:px-6 md:px-10 lg:px-20">
+  <h1 className="heading text-center">
+    My <span className="text-purple">TechStack</span>
+  </h1>
+
+  <div className="w-full py-10 flex justify-center">
+    <FloatingDock
+      mobileClassName="flex flex-wrap z-50"
+      desktopClassName=" z-50"
+      items={techstack}
+    />
+  </div>
+</div>
+
+  )
+}
+
+export default TechStack
