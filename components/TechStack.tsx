ad5b402f99d256d1c56d8a79b5495641e7bb4f3d
aryan-prog <mandelakalam12364@gmail.com>
2025-04-26 15:26:51 +0530

    experience added

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { FloatingDock } from './ui/FloatingDock';
+import React from "react";
+import { FloatingDock } from "./ui/FloatingDock";
 import { techstack } from "@/data";
 
 const TechStack = () => {
   return (
-    <div id="techstack"className="w-full py-20 px-4 sm:px-6 md:px-10 lg:px-20">
-  <h1 className="heading text-center">
-    My <span className="text-purple">TechStack</span>
-  </h1>
+    <div id="techstack" className="w-full py-20 px-4 sm:px-6 md:px-10 lg:px-20">
+      <h1 className="heading text-center">
+        My <span className="text-purple">TechStack</span>
+      </h1>
 
-  <div className="w-full py-10 flex justify-center">
-    <FloatingDock
-      mobileClassName="flex flex-wrap z-50"
-      desktopClassName=" z-50"
-      items={techstack}
-    />
-  </div>
-</div>
+      <div className="w-full py-10 flex justify-center">
+        <FloatingDock
+          mobileClassName="flex flex-wrap z-50"
+          desktopClassName=" z-50"
+          items={techstack}
+        />
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default TechStack
+export default TechStack;
